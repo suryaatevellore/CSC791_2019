2d1c0456e073a05b3347d678a7f83f389a3f6cee
Surya Ahuja <surya.ahuja@outlook.com>
2019-04-06 16:20:10 -0400

    created functions

--- adjacency_matrix.py
+++ adjacency_matrix.py
@@ -1,10 +1,19 @@
 from pathlib import Path
 
-data_folder = Path("/home/RND-TOOL/rnd_lab/")
 
-FILENAME = 'connectivity_map.txt'
-FILEPATH = data_folder / FILENAME
+def create_connections(FILEPATH):
 
 connections = {}
 with open(FILEPATH, "r+"):
+
+
+def main():
+    data_folder = Path("/home/RND-TOOL/rnd_lab/")
+    FILENAME = 'connectivity_map.txt'
+    FILEPATH = data_folder / FILENAME
+    create_connections(FILEPATH)
+
+
+if __name__=='__main__':
+    main()
 
