3de9f7e22564170ad18029f84744391525978f8d
Surya <suryaatvellore@gmail.com>
2019-04-06 16:56:27 -0700

    Worked out neighbors of all hosts

--- adjacency_matrix.py
+++ adjacency_matrix.py
@@ -3,9 +3,19 @@ from pathlib import Path
 
 def create_connections(FILEPATH):
 
-connections = {}
-with open(FILEPATH, "r+"):
+    connections = {}
+    with open(FILEPATH, "r+") as file:
+        for line in file:
+            
+            args = line.strip().split()
+            if len(args)>5:
+                # arguments in the form of (destination, local port, dest port)
+                if args[0] in connections.keys():
+                    connections[args[0]].append((args[4], args[1], args[5]))
+                else:
+                    connections[args[0]] = []
 
+    print(connections)
 
 def main():
     data_folder = Path("/home/RND-TOOL/rnd_lab/")
