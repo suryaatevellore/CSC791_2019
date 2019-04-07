0944c1ad9235ef2443edc5b3da244f98243ddc71
Surya Ahuja <surya.ahuja@outlook.com>
2019-04-06 21:48:23 -0400

    minor changes

--- helper_functions.py
+++ helper_functions.py
@@ -1,21 +1,18 @@
 import sys
 from common_functions import get_docker_ips
 
+
 def handle_RR(device_list, index):
     RR_choice = input("Do you need RR's in your topology(y/n): ")
-    recommended_RR = len(device_list)//2
+    recommended_RR = len(device_list) // 2
     if RR_choice == 'y' or RR_choice == 'Y':
         number_of_RRs = input(f"How many RRs do you want in your topology(recommended is {recommended_RR}) ?: ")
+        return ['RR' + str(i) for i in range(1, number_of_RRs)]
     elif RR_choice == 'n' or RR_choice == 'N':
-        continue
+        return None
     else:
         print("Incorrect choice")
         sys.exit(1)
-
-    if number_of_RRs>0:
-        return ['RR' + str(i) for i in range(1, number_of_RRs)]
-    else:
-        return None
 
 
 def get_RR_IPs(RR):
