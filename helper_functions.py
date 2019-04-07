4e27bbe467d46cfb4767f3af061c90a82703ebcb
Surya Ahuja <surya.ahuja@outlook.com>
2019-04-06 21:53:34 -0400

    numberofRR should be integer

--- helper_functions.py
+++ helper_functions.py
@@ -6,7 +6,7 @@ def handle_RR(device_list, index):
     RR_choice = input("Do you need RR's in your topology(y/n): ")
     recommended_RR = len(device_list) // 2
     if RR_choice == 'y' or RR_choice == 'Y':
-        number_of_RRs = input(f"How many RRs do you want in your topology(recommended is {recommended_RR}) ?: ")
+        number_of_RRs = int(input(f"How many RRs do you want in your topology(recommended is {recommended_RR}) ?: "))
         return ['RR' + str(i) for i in range(1, number_of_RRs)]
     elif RR_choice == 'n' or RR_choice == 'N':
         return None
