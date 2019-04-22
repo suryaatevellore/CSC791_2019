f4d6257864bc02d27174ead2495fcddad82ea571
Surya Ahuja <surya.ahuja@outlook.com>
2019-04-22 11:17:29 -0400

    Removing the functions for cli level configuration

--- docker_cmd_config.py
+++ docker_cmd_config.py
@@ -1,13 +1,3 @@
 import os
-import subprocess
-from adjacency_matrix import create_neighbors, host_mapping
 import time
 
-
-def common_terminal_config(device_list, Loopbacks=None, device_ip=None):
-    print("Received list and loopbacks {device_list} {Loopbacks}")
-    for device in device_list:
-        print(f"Configuring loopbacks on {device}")
-        os.system(f"sudo docker exec -d {device} bash -c 'apt-get install bridge-utils -y'")
-        os.system(f"sudo docker exec -d {device} bash -c 'ip addr add {Loopbacks[device]}/32 dev lo'")
-        time.sleep(0.5)
